b365a0c89f582385db107ed7f6d010258de6ba13
Gregoire <gregoire.cottin@skezi.eu>
2023-05-28 16:53:43 +0200

    feat(stream): posts new releases of a github repo

---
+++ github/config.ts
@@ -0,0 +1,9 @@
+import { RepoInfo } from "./types";
+
+export const repos: RepoInfo[] = [
+    {
+        owner: 'solana-labs',
+        repo: 'solana',
+        lastReleaseDate: undefined,
+    },
+]
--- module/feedApp.ts
+++ module/feedApp.ts
@@ -2,27 +2,41 @@ import { initDiscord } from "../discord/client";
 import { getQuestions } from "../stack-exchange-api/getQuestions";
 import { createPostOnDiscordChannel } from "../discord/createPostOnDiscordChannel";
 import { initTelegram } from "../telegram/client";
+import { repos } from "../github/config";
+import githubApi from "../github/api";
 
 (async () => {
-    try{
+    try {
         // Init discord client
         const client = await initDiscord();
+        const channelId = process.env.DISCORD_CHANNEL_ID as string;
 
         // Init telegram bot
         let bot = initTelegram()
 
-
+        const intervalSeconds = 300;
         setInterval(async () => {
             // Get questions from StackExchange API
-            const questions = await getQuestions(5, 'solana')
-            
-            // Post questions on Discord
+            const questions = await getQuestions(intervalSeconds, 'solana')
+
+            // Post questions on Discord and Telegram
             questions.forEach(async (question) => {
-                await createPostOnDiscordChannel(client, question)
+                await createPostOnDiscordChannel(client, channelId, { question })
                 bot?.sendMessage(process.env.TELEGRAM_CHAT_ID || '', question.link)
             })
-        }, 300000)
-    }catch(error){
+
+            for (let repo of repos) {
+                // Get releases of each repo from github API
+                const releases = await githubApi.getReleases(repo);
+
+                // Post each release on Discord and Telegram
+                for (let release of releases) {
+                    await createPostOnDiscordChannel(client, channelId, { release })
+                    bot?.sendMessage(process.env.TELEGRAM_CHAT_ID || '', release.url)
+                }
+            }
+        }, intervalSeconds * 1000);
+    } catch (error) {
         console.log(error)
     }
 })()
